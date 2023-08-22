20106281ccd668281db13430958fa8aa1296e641
dadav <33197631+dadav@users.noreply.github.com>
2023-08-22 23:48:36 +0200

    fix: use structs to parse the schema

--- pkg/schema/schema_test.go
+++ pkg/schema/schema_test.go
@@ -9,32 +9,26 @@ func TestValidate(t *testing.T) {
 	}{
 		{
 			comment:       "foo",
-			expectedValid: false,
-		},
-		{
-			comment:       "# @schema type=string",
 			expectedValid: true,
 		},
 		{
-			comment:       "# @schema type=string type=integer",
+			comment: `
+      # @schema
+      # multipleOf: true
+      # @schema`,
 			expectedValid: false,
 		},
 		{
-			comment:       "# @schema type=string min=1",
-			expectedValid: false,
-		},
-		{
-			comment:       "# @schema type=object min=1",
-			expectedValid: false,
-		},
-		{
-			comment:       "# @schema type=doesntexist",
+			comment: `
+      # @schema
+      # type: doesnotexist
+      # @schema`,
 			expectedValid: false,
 		},
 	}
 
 	for _, test := range tests {
-		schemas, _, err := GetSchemasFromComment(test.comment)
+		schema, _, err := GetSchemaFromComment(test.comment)
 		if err != nil && test.expectedValid {
 			t.Errorf(
 				"Expected the schema %s to be valid=%t, but can't even parse it: %v",
@@ -43,18 +37,15 @@ func TestValidate(t *testing.T) {
 				err,
 			)
 		}
-		for i, schema := range schemas {
-			err := schema.Validate()
-			valid := err == nil
-			if valid != test.expectedValid {
-				t.Errorf(
-					"Expected schema %s (number %d) to be valid=%t, but it's %t",
-					test.comment,
-					i,
-					test.expectedValid,
-					valid,
-				)
-			}
+		err = schema.Validate()
+		valid := err == nil
+		if valid != test.expectedValid {
+			t.Errorf(
+				"Expected schema %s to be valid=%t, but it's %t",
+				test.comment,
+				test.expectedValid,
+				valid,
+			)
 		}
 	}
 }
