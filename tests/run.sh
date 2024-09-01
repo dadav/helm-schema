6c999cd76bb6747a9e4048b8e5985ee114fa0879
dadav <33197631+dadav@users.noreply.github.com>
2024-09-01 21:09:33 +0200

    feat: Add e2e tests

---
+++ tests/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rc=0
+
+for test_file in test_*.yaml; do
+  expected_file="${test_file%.yaml}_expected.schema.json"
+  generated_file="${test_file%.yaml}_generated.schema.json"
+  ./helm-schema -f "$test_file" -o "$generated_file"
+  echo "Testing $test_file"
+  if diff -y <(jq --sort-keys . "$generated_file") <(jq --sort-keys . "$expected_file"); then
+    echo "✅: $test_file"
+  else
+    echo "❌: $test_file"
+    rc=1
+  fi
+done
+
+exit "$rc"
