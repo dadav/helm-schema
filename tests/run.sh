c20d8329d4e2b5b5fe84937c65ae94e59deeaf76
dadav <33197631+dadav@users.noreply.github.com>
2024-12-30 19:42:19 +0100

    fix: Decrease verbosity

--- tests/run.sh
+++ tests/run.sh
@@ -6,20 +6,20 @@ cp ../examples/values.yaml test_repo_example.yaml
 cp ../examples/values.schema.json test_repo_example_expected.schema.json
 
 for test_file in test_*.yaml; do
-  expected_file="${test_file%.yaml}_expected.schema.json"
-  generated_file="${test_file%.yaml}_generated.schema.json"
-  if ! ./helm-schema -f "$test_file" -o "$generated_file"; then
-    echo "❌: $test_file"
-    rc=1
-    continue
-  fi
-  echo "Testing $test_file"
-  if diff -y <(jq --sort-keys . "$generated_file") <(jq --sort-keys . "$expected_file"); then
-    echo "✅: $test_file"
-  else
-    echo "❌: $test_file"
-    rc=1
-  fi
+	expected_file="${test_file%.yaml}_expected.schema.json"
+	generated_file="${test_file%.yaml}_generated.schema.json"
+	if ! ./helm-schema -f "$test_file" -o "$generated_file"; then
+		echo "❌: $test_file"
+		rc=1
+		continue
+	fi
+	echo "Testing $test_file"
+	if diff -y --suppress-common-lines <(jq --sort-keys . "$generated_file") <(jq --sort-keys . "$expected_file"); then
+		echo "✅: $test_file"
+	else
+		echo "❌: $test_file"
+		rc=1
+	fi
 done
 
 exit "$rc"
